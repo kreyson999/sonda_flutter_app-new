a4be76788fd8c599d6a2a8c264dcefaaabab6061
kreyson999 <mlynekjakub05@gmail.com>
2023-04-06 22:56:41 +0200

    cosmetic changes in arduino main

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -8,7 +8,6 @@ const int stepsPerRevolution = 200;
 SoftwareSerial MyBlue(2, 3); //rx2, tx3
 Stepper myStepper(stepsPerRevolution, 4, 5, 6, 7);
 const unsigned int MAX_MESSAGE_LENGTH = 12;
-int previous = 0;
 
 #define phSensorPin A0
 #define Offset 0.00
@@ -32,41 +31,37 @@ void setup() {
 void loop() {
   while (MyBlue.available() > 0)
   {
-    //Create a place to hold the incoming message
     static char message[MAX_MESSAGE_LENGTH];
     static unsigned int message_pos = 0;
-
-    //Read the next available byte in the serial receive buffer
+    
     char inByte = MyBlue.read();
-
-    //Message coming in (check not terminating character) and guard for over message size
     if ( inByte != '\n' && (message_pos < MAX_MESSAGE_LENGTH - 1) )
     {
-      //Add the incoming byte to our message
       message[message_pos] = inByte;
       message_pos++;
     }
-    //Full message received...
+    // Gdy odebrana cala wiadomosc
     else
     {
-      //Add null character to string
       message[message_pos] = '\0';
 
-      //Print the message (or do other things)
-      // CPH - czujnik PH w aplikacji
-      Serial.println("odczytuje dane");
       if (strcmp(message, "read") == 0) {
+        // CPH - czujnik PH w aplikacji
+        Serial.println("Wysylam czujniki");
         MyBlue.print("CPH ");
         MyBlue.print(phSensor());
         MyBlue.print("\n");
       }
+      // podnies kabel
       if (strcmp(message, "up") == 0) {
+        Serial.println("Podnosze kabel");
         myStepper.step(-stepsPerRevolution);
       }
+      // opusc kabel
       if (strcmp(message, "down") == 0) {
+        Serial.println("Opuszczam kabel");
         myStepper.step(-stepsPerRevolution);
       }
-      //Reset for the next message
       message_pos = 0;
     }
   }
